f5b8cd3aa9202f9b78b4e2ce9bde530c1d5b5300
MariadelosAngelessolishernandez <146492483+MariadelosAngelessolishernandez@users.noreply.github.com>
2023-10-01 23:22:35 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//comentarios
+/*Comentarios en bloque:
+ * Console.writwline es para imprimir
+ * para imprimir texto sin variable
+ * tiene que ir entre" " doble*/
+//cada line debe finalizar con punto y coma;
+using System; //paso1
+namespace programa1//aso1
+{
+    internal class program //paso1
+    {
+        static void Main(string[] args)//paso1
+        {
+            //DECLARAR VARIABLES
+            string nombre = ("Maria de los Angeles");
+            //imprimir nombre referenciado a una variable
+            Console.WriteLine(nombre);
+            //cada Linea debe finalizar con ; punto y coma
+        }
+    }
+}
