ba445c76693a3b8db5e6264daf12e0812f14759a
MariadelosAngelessolishernandez <146492483+MariadelosAngelessolishernandez@users.noreply.github.com>
2023-10-17 22:51:48 -0500

    U3program5,descuento

---
+++ unidad3/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace U3program5
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, p3, total;
+            Console.Write("Escribe el precio 1: ");
+            p1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escribe el precio 2: ");
+            p2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escribe el precio 3: ");
+            p3 = Convert.ToDouble(Console.ReadLine());
+            total = p1 + p2 + p3;
+            if (total >= 1500 ) {
+                total = total - (total * 0.30);
+                Console.WriteLine("El total (30% desc):" + total);
+            } else if (total < 1500 && total >= 1000) {
+                total = total - (total * 0.20);
+                Console.WriteLine("El total (20% desc):" + total);
+            } else if (total < 1000 && total >= 700)
+            {
+                total = total - (total * 0.10);
+                Console.WriteLine("El total (10% desc):" + total);
+            }
+            else
+            {
+                Console.WriteLine("El total(sin desc): " + total);
+
+
+            }
+        }
+    }
+}
