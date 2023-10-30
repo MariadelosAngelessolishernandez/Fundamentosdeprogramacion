27670af4a29ca4394b4c5a8b22b64e7456baa44e
MariadelosAngelessolishernandez <146492483+MariadelosAngelessolishernandez@users.noreply.github.com>
2023-10-30 18:09:27 -0500

    c#

---
+++ c#U3/ProgramBienvenida.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PPROMEDIO2
+{
+    class Programa1u3
+    {
+        static void Main(string[] args)
+        {
+            char opcion;
+            do
+            {
+                Console.Write("Bienvenida a una sola ejecucion");
+                Console.Write(" Escribe n para salir!!");
+                opcion = Console.ReadKey().KeyChar;
+                Console.Write(opcion);
+
+            } while (opcion != 'n');
+
+        }
+    }
+}
---
+++ c#U3/Programa20Numeros.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PPROMEDIO2
+{
+    class Programa1u3
+    {
+        static void Main(string[] args)
+        {
+            int c = 1, horas, pago, sueldo;
+            while (c <= 20)
+            {
+                Console.Write("captura las horas trabajadas");
+                horas = Convert.ToInt32(Console.ReadLine());
+                Console.Write("captura del pago x hora: ");
+                pago = Convert.ToInt32(Console.ReadLine());
+                sueldo = horas * pago;
+                Console.Write("Tusueldo es:" + sueldo);
+                c++;
+            }
+        }
+    }
+}
