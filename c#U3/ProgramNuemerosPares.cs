56411a56ee1bbb9d2a85b8fd095538da984f7459
MariadelosAngelessolishernandez <146492483+MariadelosAngelessolishernandez@users.noreply.github.com>
2023-10-30 17:50:14 -0500

    Add files via upload

---
+++ c#U3/ProgramNuemerosPares.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PPROMEDIO2
+{
+    class Programa1u3
+    {
+        static void Main(string[] args)
+        {
+            int np = 0;
+            int nn = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                if (numero > 0)
+                {
+                    np++;
+                }
+                else if (numero < 0)
+                {
+                    nn++;
+                }
+            }
+
+            Console.WriteLine("Números positivos: " + np);
+            Console.WriteLine("Números negativos: " + nn);
+        }
+    }
+
+    
+}
+
+
