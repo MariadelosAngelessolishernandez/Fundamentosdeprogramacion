43885a699546db3dd073ba7102c01abfb0647856
MariadelosAngelessolishernandez <146492483+MariadelosAngelessolishernandez@users.noreply.github.com>
2023-10-30 17:48:46 -0500

    Add files via upload

---
+++ c#U3/ProgramCienNPares.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Pdescuento
+{
+    class Programa4u3
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 200; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ c#U3/ProgramaSalario10%.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Pdescuento
+{
+    class Programa4u3
+    {
+        static void Main(string[] args)
+        {
+
+            double salario = 1500;
+            double incremento = 0.10;
+            double salarioActual;
+            for (int i = 1; i <= 6; i++)
+            {
+
+                salarioActual = salario + salario * incremento;
+
+                Console.WriteLine("Año " + i + ": $" + salarioActual);
+
+                salario = salarioActual;
+            }
+
+            Console.WriteLine("Salario después de 6 años: $" + salario);
+        }
+    }
+}
+
